0bd3d75e79f9b1de8b58ad9a2caff00e06a43fd7
kabirshuvo <kabirshuvo19@gmail.com>
2023-03-28 00:41:57 +0600

    Grand Total Added

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,21 +3,27 @@ import './Cart.css';
 
 const Cart = ({cart}) => {
     //console.log(cart)
-    let total = 0;
+    let totalPrice = 0;
     //console.log(total);
+    let shippingCost = 0;
+   
 
     for(const product of cart){
-        total = total+product.price;
+        totalPrice = totalPrice+product.price;
+        shippingCost = shippingCost + product.shipping;
+        
     }
+    let tax = totalPrice*3/100;
+    const grandTotal = totalPrice + shippingCost + tax;
     //console.log(total);
     return (
         <div className='cart'>
             <h4>Order Summary</h4>
             <p>Sellected Items {cart.length}</p>
-            <p>Total Price: {total} </p>
-            <p>Total Shipping: </p>
-            <p>Tax: </p>
-            <h6>Grand Total: </h6>
+            <p>Total Price: $ <span className='total'>{totalPrice.toFixed(2)}</span> </p>
+            <p>Total Shipping: $ {shippingCost.toFixed(1)}</p>
+            <p>Tax: $ {tax.toFixed(1)}</p>
+            <h6>Grand Total: $ {grandTotal.toFixed(2)}</h6>
         </div>
     );
 };
